8db66a3e001de7553ffe898ee12ab7aa35a8f149
Gonzalo Mata <gonzalomatah96@gmail.com>
2015-03-06 14:55:19 -0600

    WSQ09 complete
    I added a conditional to get the true value of 0 factorial.

--- WSQ09.cpp
+++ WSQ09.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-//#include "BigIntegerLibrary.hh"
+#include "BigIntegerLibrary.hh"
 using namespace std;
 
 int main(){
 
-  int n, x;
+  long long int n, x;
   char answer, yes = 'y', no = 'n';
 
 
@@ -23,15 +23,20 @@ do {
 
   while (x > 1){
     x = x - 1;  //this while makes the factorial operation Ex: n=3
-    n = n * x;  //now x=2, so: 3=3*2 and it returns to x=x-1 and x will be 1 and the loop ends
+    n = n * x;  //now x=3-1 that's 1, so: 3=3*2 and it returns to x=x-1 and x will be 1 and the loop ends
   }  /* why x>1? because if you have x>0 as conditional the last number in multiplication
   will be 0 and the answer will be 0, but with this conditional the last number
   to multiplicate left will be 1*/
+
+  if (n == 0){
+    n = 1;
+  } //This if gives to n the real value of fatorial of 0
 
   cout << "The factorial of you number is..." << endl << n << endl;
 
   cout << "Do you want to try with another number? ( yes = y/ no = n )" << endl;
   cin >> answer;
+
 
 } while (answer == yes);
 
