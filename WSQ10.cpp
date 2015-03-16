25964d6e4185df4d5c7fd1353caf89faa0b30c02
Gonzalo Mata <gonzalomatah96@gmail.com>
2015-03-16 12:33:37 -0600

    2 things left: standard deviation and list of numbers
    code almost done.

--- WSQ10.cpp
+++ WSQ10.cpp
@@ -3,5 +3,27 @@ using namespace std;
 
 int main (){
 
+  int size, count=0;
+  float usrnumber, sum=0;
+
+  cout << "Give me the number of spaces for your list: " << endl;
+  cin >> size;
+  cout << "------------------" << endl;
+  float array[size];
+
+  for(int i = 0; i < size ; i++){
+    count = count+1;
+    cout << "Give me the number to place " << i + 1 << endl;
+    cin >> usrnumber;
+    array[i] = usrnumber;
+    sum = sum + usrnumber; //This part makes the sum each time when the user puts a new number.
+  }
+
+  cout << endl << "Your numbers are: " << endl;
+  cout << count;
+  cout << "The sum of your numbers is: " << sum << endl;
+  cout << "The average of your numbers is: " << sum/size << endl;
+  cout << "The standard deviation of those numbers is: " << endl;
+
   return 0;
 }
