6034a4781edcfd00b03c5c810e5f7fecaaa0b67d
Gonzalo Mata <gonzalomatah96@gmail.com>
2015-03-17 10:54:23 -0600

    Code done.
    The only thing left is the way of program. I’m thinking on introduce
    some functions to do all the math stuff

--- WSQ10.cpp
+++ WSQ10.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 int main (){
 
   int size, count=0;
-  float usrnumber, sum=0;
+  float usrnumber, sum=0.0, var=0, average, deviation, variation;
 
   cout << "Give me the number of spaces for your list: " << endl;
   cin >> size;
@@ -12,18 +13,34 @@ int main (){
   float array[size];
 
   for(int i = 0; i < size ; i++){
-    count = count+1;
     cout << "Give me the number to place " << i + 1 << endl;
     cin >> usrnumber;
     array[i] = usrnumber;
-    sum = sum + usrnumber; //This part makes the sum each time when the user puts a new number.
+    sum = sum + usrnumber; /*This part makes the sum each time
+    when the user puts a new number*/
   }
 
+  average = sum/size;
+
   cout << endl << "Your numbers are: " << endl;
-  cout << count;
+
+  for(int i=0; i < size; i++) {
+    count = count + 1;
+    cout << count << ".- " << array[i] << endl;
+  }/*This loop makes the list of the numbers into the arrays*/
+
   cout << "The sum of your numbers is: " << sum << endl;
-  cout << "The average of your numbers is: " << sum/size << endl;
-  cout << "The standard deviation of those numbers is: " << endl;
+  cout << "The average of your numbers is: " << average << endl;
+
+  for(int i=(size-1); i>=0; --i){
+    var = ((array[i]-average)*(array[i]-average) + var);
+  } /*This loop makes something call "varianza" of the data, then it will have
+  a square root to make it the standard deviation*/
+
+  variation = var/size;
+  deviation = sqrt(variation);
+
+  cout << "The standard deviation of those numbers is: " << deviation << endl;
 
   return 0;
 }
