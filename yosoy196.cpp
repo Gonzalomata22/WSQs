c22c3b3f9c83256d9e7d62725e2f72353339fdc7
Gonzalo Mata <gonzalomatah96@gmail.com>
2015-04-02 11:51:41 -0600

    Update

--- yosoy196.cpp
+++ yosoy196.cpp
@@ -3,56 +3,59 @@
 //
 // Gonzalo Mata
 // A01630049
-//
 #include <string>
 #include <iostream>
-#include "BigIntegerLibrary.hh"
-
 using namespace std;
 
-/*
- * This function receives a BigInteger n and checks if it is a palindrome
- * Return true if n is a palindrome, return false otherwise
- */
+#include "BigIntegerLibrary.hh"
 
-bool is_palindrome(BigInteger n){
-  int candidato;
+bool is_palindrome(int n){
   string x = to_string (n);
   reverse(x.begin(),x.end());
   int y = stoi(x);
-  candidato = n + y;
-  if 
-  return true; // You need to fix this, this is not right
+  if (n == y){
+    return true;
+  } else{
+      return false;
+  }
 }
 
-/*
- * This function receives a BigInteger n and returns the value of n plus the inverse of n.
- */
-BigInteger apply196(BigInteger n){
+int apply196(int n){
+  int candidato, candidato2;
 
-  return n; // you need to replace this with code that works
-}
-
-BigInteger invert(int n){
   string x = to_string (n);
   reverse(x.begin(),x.end());
   int y = stoi(x);
-  return y;
+
+  candidato = y + n;
+
+  return candidato;
 }
 
-
-/*
- * Your main program goes below and should use the two functions above
- */
 int main() {
-  int lower, high, counter = 0;
+  int low, high, counterpalindrome = 0, becomepalindrome = 0, Lychrelcounter = 0;
+
   cout << "Introduce el primer numero del rango." << endl;
-  cin >> lower;
+  cin >> low;
   cout << "Introduce el segundo numero del rango." << endl;
   cin >> high;
 
-  is_palindrome(lower);
-  is_palindrome(high);
+  for(int i=low; i<=high; i++){
+    if(is_palindrome(i) == true){
+      counterpalindrome = counterpalindrome + 1;
+    }
+    if (apply196(i) == i){
+      becomepalindrome = becomepalindrome + 1;
+    } else{
+      Lychrelcounter++;
+      cout << "Encontre un numero de Lychrel: " << i << endl;
+    }
+  }
+
+  cout << "El rango de numeros analizados fue de " << (high - low) + 1 << " numeros." << endl;
+  cout << "Encontre " << counterpalindrome << "numeros que son palindromos naturales." << endl;
+  cout << "Encontre " << becomepalindrome << " numeros que no son numeros de Lychrel." << endl;
+  cout << "Encontre " << Lychrelcounter << " numeros de Lychrel." << endl;
 
   return 0;
 }
